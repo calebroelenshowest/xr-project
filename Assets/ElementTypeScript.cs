858d49d7fa3c0072b0759e81b7991d6709be983f
calebroelenshowest <55787034+calebroelenshowest@users.noreply.github.com>
2021-11-02 10:09:38 +0100

    Added holding & release states

--- Assets/ElementTypeScript.cs
+++ Assets/ElementTypeScript.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.Utilities;
 using UnityEngine;
 
 public class ElementTypeScript : MonoBehaviour
@@ -10,11 +13,12 @@ public class ElementTypeScript : MonoBehaviour
     [SerializeField] public bool isFinal;
     [SerializeField] public Material combinationMaterial;
     [SerializeField] public Material resultMaterial;
+    [SerializeField] public bool holoHolding = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        GameObject child = this.gameObject.transform.GetChild(0).gameObject;
+        GameObject child = gameObject.transform.GetChild(0).gameObject;
         
         if (!unlocked)
         { 
@@ -23,6 +27,31 @@ public class ElementTypeScript : MonoBehaviour
         else
         {
             child.GetComponent<Renderer>().material = elementMaterial;
+        }
+    }
+
+    public void HoldObject()
+    {
+        Debug.Log("Changed holoHolding state to true");
+        holoHolding = true;
+    }
+
+    public void ReleaseObject()
+    {
+        Debug.Log("Changed holoHolding state to false");
+        holoHolding = false;
+    }
+
+    public void OnCollisionStay(Collision other)
+    {
+        if (holoHolding)
+        {
+            Debug.Log("Holding");
+            // Do nothing: Only combine elements when released
+        }
+        else
+        {
+            Debug.Log("Not holding");
         }
     }
 
