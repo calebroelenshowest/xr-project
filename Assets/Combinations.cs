2c3b03304f00960a9245096657bb13021e02f164
calebroelenshowest <55787034+calebroelenshowest@users.noreply.github.com>
2021-11-15 11:15:43 +0100

    Added some code

---
+++ Assets/Combinations.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.MixedReality.Toolkit.Rendering;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using UnityEngine;
+using UnityEngine.AI;
+
+[System.Serializable]
+public class ElementCombination
+{
+    public Material elementMaterial;
+    public GameObject resultPrefab;
+}
+
+public class Combinations : MonoBehaviour
+{
+    [SerializeField] public Material displayMaterial;
+    [SerializeField]
+    public List<ElementCombination> elementCombinations;
+
+    private bool _holdingOjbect;
+    [NonSerialized]
+    public bool onCollisionStayBehind = false;
+    void Start()
+    {
+        gameObject.transform.GetChild(0).gameObject.GetComponent<Renderer>().material = displayMaterial;
+    }
+    
+    void Update()
+    {
+        // Update method : Update once every frame
+        
+    }
+
+    public void HoldObject()
+    {
+        Debug.Log("Holding this object with hand.");
+        _holdingOjbect = true;
+    }
+
+    public void ReleaseObject()
+    {
+        Debug.Log("Released this object with hand. No longer holding this.");
+        _holdingOjbect = false;
+    }
+
+    // On collision
+    private void OnCollisionStay(Collision collisionElement)
+    {
+        // Does the object have the same tag: Element
+        if (collisionElement.gameObject.CompareTag(gameObject.tag))
+        {
+            // If still holding the object --> Do not combine
+            if (_holdingOjbect) return;
+            // If not holding --> Combine both elements if possible
+            Material collisionMaterial = collisionElement.gameObject.GetComponent<Renderer>().material;
+            // Cannot combine with itself.
+            if (collisionMaterial == gameObject.GetComponent<Renderer>().material) return;
+            if (elementCombinations.Count == 0) return; // This is a final element --> No elements inside
+            bool foundMatching = false;
+            foreach(ElementCombination combination in elementCombinations)
+            {
+                if (combination.elementMaterial == collisionMaterial)
+                {
+                    foundMatching = true;
+                    // Destroy colling object and remove existing collider on existing object.
+                    if (onCollisionStayBehind) return;
+                    collisionElement.gameObject.GetComponent<Combinations>().onCollisionStayBehind = true;
+                    Destroy(collisionElement.gameObject);
+                    gameObject.GetComponent<Collider>().enabled = false;
+                    // Spawn the resulting combination
+                    int index = elementCombinations.IndexOf(combination);
+                    Instantiate(elementCombinations[index].resultPrefab);
+                    // Remove the existing element
+                    Destroy(gameObject);
+                    
+                }
+            }
+
+            // No matching material? Cancel!
+            if (!foundMatching) return;
+            
+        }
+        else
+        {
+            // Combination of elements not allowed.
+            return;
+        }
+    }
+}
