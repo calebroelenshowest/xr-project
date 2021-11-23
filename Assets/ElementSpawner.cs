4339308d038c1d4d857a5d20ebcfaadebb24fd0e
calebroelenshowest <55787034+calebroelenshowest@users.noreply.github.com>
2021-11-23 13:54:28 +0100

    Added files

---
+++ Assets/ElementSpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json.Serialization;
+using UnityEngine;
+
+[SerializeField]
+public class ElementSpawnerLocations
+{
+    public GameObject gameObjectToSpawn;
+    public bool unlocked;
+}
+
+
+public class ElementSpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public ElementSpawnerLocations[] Locations;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
